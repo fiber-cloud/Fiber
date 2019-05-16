240ef6070b01adb7141e9411e1d61a9d2cf6cf3e
Tammo0987 <tammo.steffens@googlemail.com>
2019-05-16 18:06:18 +0200

    Configured the task dependencies for building the jar

--- Fiber-Node/build.gradle.kts
+++ Fiber-Node/build.gradle.kts
@@ -1,3 +1,12 @@
 dependencies {
     implementation(Dependencies.kotlinStandard)
 }
+
+tasks {
+    val collectDependencies by registering(DependencyTask::class) {
+        this.group = "fiber"
+        this.description = "Collect all dependencies for the runtime in the dependencies.json file"
+    }
+
+    this.findByName("build")!!.dependsOn(collectDependencies)
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -17,12 +17,7 @@ subprojects {
         mavenCentral()
     }
 
-    tasks.withType<KotlinCompile> {
+    this.tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
     }
-}
-
-this.tasks.register<DependencyTask>("collectDependencies") {
-    this.group = "fiber"
-    this.description = "Collect all dependencies for the runtime in the dependencies.json file"
 }
