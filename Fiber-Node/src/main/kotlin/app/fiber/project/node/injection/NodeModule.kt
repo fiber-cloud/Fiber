0022423d7496404dcd690ef2974b8d89d42e30bb
x7airworker <jake@jakes-co.de>
2019-05-20 17:33:08 +0200

    Added EventBus, but there are still unchecked casts

--- Fiber-Node/src/main/kotlin/app/fiber/project/node/injection/NodeModule.kt
+++ Fiber-Node/src/main/kotlin/app/fiber/project/node/injection/NodeModule.kt
@@ -1,10 +1,12 @@
 package app.fiber.project.node.injection
 
 import app.fiber.project.node.addon.AddonManager
+import app.fiber.project.node.event.EventBus
 import app.fiber.project.node.logging.Logger
 import org.koin.dsl.module
 
 val nodeModule = module {
     single { Logger() }
     single { AddonManager(get()) }
+    single { EventBus() }
 }
