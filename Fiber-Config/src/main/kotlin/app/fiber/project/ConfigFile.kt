63d39953aaf742e4e8e5b3aeedf4ea73dbc61351
Tammo0987 <tammo.steffens@googlemail.com>
2019-05-22 17:01:09 +0200

    Fixed some little things in ConfigFile

--- Fiber-Config/src/main/kotlin/app/fiber/project/ConfigFile.kt
+++ Fiber-Config/src/main/kotlin/app/fiber/project/ConfigFile.kt
@@ -1,11 +1,13 @@
 package app.fiber.project
 
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 
 abstract class ConfigFile(name: String) {
 
-    protected val path = Paths.get("config/$name.yaml")
+    @Suppress("MemberVisibilityCanBePrivate")
+    protected val path: Path = Paths.get("config/$name.yaml")
 
     init {
         if (Files.notExists(this.path.parent)) Files.createDirectories(this.path.parent)
