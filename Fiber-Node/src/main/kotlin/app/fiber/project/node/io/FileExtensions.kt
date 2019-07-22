bf13cbc6af5a83bd4b712b06f1c9bc7a8ed23eae
Tammo0987 <tammo.steffens@googlemail.com>
2019-07-22 17:25:41 +0200

    Improved file extensions

--- Fiber-Node/src/main/kotlin/app/fiber/project/node/io/FileExtensions.kt
+++ Fiber-Node/src/main/kotlin/app/fiber/project/node/io/FileExtensions.kt
@@ -2,8 +2,6 @@ package app.fiber.project.node.io
 
 import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.file.Paths
-import java.nio.file.StandardCopyOption
 
 fun Path.exists(): Boolean = Files.exists(this)
 
@@ -11,26 +9,10 @@ fun Path.createDirectories(): Path = Files.createDirectories(this)
 
 fun Path.createFile(): Path = Files.createFile(this)
 
-fun Path.listFiles(): List<Path> = this.toFile().listFiles().map { it.toPath() }
+fun Path.listFiles(): List<Path> = (this.toFile().listFiles()?: emptyList()).map { it.toPath() }
 
 fun Path.isDirectory(): Boolean = this.toFile().isDirectory
 
-fun Path.copy(destination: Path) {
-    if (!destination.exists()) destination.createDirectories()
+fun Path.copy(destination: Path) = this.toFile().copyRecursively(destination.toFile(), true)
 
-    //TODO streams?
-    this.listFiles().forEach {
-        if (it.isDirectory()) {
-            it.copy(Paths.get(it.toAbsolutePath().toString(), it.toString()))
-        } else {
-            Files.copy(it, Paths.get(destination.toAbsolutePath().toString(), it.toString()), StandardCopyOption.REPLACE_EXISTING)
-        }
-    }
-
-}
-
-fun Path.delete() {
-    Files.walk(this)
-        .sorted(Comparator.reverseOrder())
-        .forEach { Files.delete(it) }
-}
+fun Path.delete() = this.toFile().deleteRecursively()
