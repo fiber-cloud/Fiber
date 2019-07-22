818a9b26d847c6341cd5c3cd0b127adbae8503e5
Tammo0987 <tammo.steffens@googlemail.com>
2019-07-22 17:12:17 +0200

    Documented the CommandResult enumeration

--- Fiber-Command/src/main/kotlin/app/fiber/project/command/result/CommandResult.kt
+++ Fiber-Command/src/main/kotlin/app/fiber/project/command/result/CommandResult.kt
@@ -8,12 +8,39 @@ package app.fiber.project.command.result
  */
 enum class CommandResult {
 
+    /**
+     * Command executed successfully.
+     */
     SUCCESS,
+
+    /**
+     * Command execution failed.
+     */
     FAILED,
+
+    /**
+     * Size of parameters are not right.
+     */
     WRONG_PARAMETERS,
+
+    /**
+     * Command not exists.
+     */
     COMMAND_NOT_FOUND,
+
+    /**
+     * Command route not found.
+     */
     ROUTE_NOT_FOUND,
+
+    /**
+     * Parameters could not be converted.
+     */
     CONVERTER_ERROR,
+
+    /**
+     * The input is not a valid command.
+     */
     UNUSABLE_INPUT
 
 }
