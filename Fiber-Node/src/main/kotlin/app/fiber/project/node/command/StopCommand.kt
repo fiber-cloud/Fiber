d35735e3d337251a2866e0573eeeef4c5b730300
Tammo0987 <tammo.steffens@googlemail.com>
2019-07-22 16:02:20 +0200

    Replacing System#exit through the kotlin method exitProcess

--- Fiber-Node/src/main/kotlin/app/fiber/project/node/command/StopCommand.kt
+++ Fiber-Node/src/main/kotlin/app/fiber/project/node/command/StopCommand.kt
@@ -3,14 +3,12 @@ package app.fiber.project.node.command
 import app.fiber.project.command.annotation.Model
 import app.fiber.project.command.annotation.Route
 import app.fiber.project.command.result.CommandResult
+import kotlin.system.exitProcess
 
 @Model("stop", "Stops the node", ["shutdown", "exit"])
 class StopCommand {
 
     @Route(description = "Stops the node")
-    fun default(): CommandResult {
-        System.exit(0)
-        return CommandResult.SUCCESS
-    }
+    fun default(): CommandResult = exitProcess(0)
 
 }
